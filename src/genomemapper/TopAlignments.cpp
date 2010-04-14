e7cc0f8fabae6292a1d90f19904c6364d143d62d
raetsch <raetsch@e1793c9e-67f9-0310-80fc-b846ff1f7b36>
2010-04-14 10:06:25 +0000

    *** empty log message ***

--- src/genomemapper/TopAlignments.cpp
+++ src/genomemapper/TopAlignments.cpp
@@ -31,7 +31,7 @@ TopAlignments::TopAlignments(GenomeMaps* genomemaps_)
 	}
 
 	genomemaps = genomemaps_ ;
-	MAX_EXON_LEN = 150 ;
+	MAX_EXON_LEN = 200 ;
 }
 
 u_int8_t TopAlignments::report_unspliced_hit(HIT *hit, int num, QPalma* qpalma) 
