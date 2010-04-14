d797ed8a9a093b9f4bbf6bbb74f3eff0889f7d4d
raetsch <raetsch@e1793c9e-67f9-0310-80fc-b846ff1f7b36>
2010-04-14 16:57:05 +0000

    added missing header

--- src/genomemapper/dyn_prog/qpalma_dp.h
+++ src/genomemapper/dyn_prog/qpalma_dp.h
@@ -7,7 +7,7 @@
 #include "penalty_info_dp.h"
 #include "fill_matrix.h"
 #include "debug_tools.h"
-
+#include "stdint.h"
 
 
 bool fast_result_align(const std::vector<SeedElem*>& seed_matrix_left, const std::vector<SeedElem*>& seed_matrix_right, int z, int est_len, int dna_len, int* result_length_ptr, 
