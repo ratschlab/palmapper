347e3e119c74af5f18e3978ccf986ec95abda5b0
raetsch <raetsch@e1793c9e-67f9-0310-80fc-b846ff1f7b36>
2010-05-02 15:54:04 +0000

    + if -H is not given (spliced read alignments), then output into unspliced file instead of stdout

--- src/palmapper/init.c
+++ src/palmapper/init.c
@@ -21,16 +21,19 @@ int init_output_file(Config * config)
 	return (0);
 }
 
-int init_spliced_output_file(Config * config) {
-	if (config->SPLICED_HITS && config->SPLICED_OUT_FILE_NAME.length() > 0) {
-		if ((SP_OUT_FP = fopen(config->SPLICED_OUT_FILE_NAME.c_str(), "w")) == NULL) {
+int init_spliced_output_file(Config * config) 
+{
+	if (config->SPLICED_HITS && config->SPLICED_OUT_FILE_NAME.length() > 0) 
+	{
+		if ((SP_OUT_FP = fopen(config->SPLICED_OUT_FILE_NAME.c_str(), "w")) == NULL) 
+		{
 			fprintf(stderr,
 					"ERROR : Couldn't open output file for spliced hits %s\n",
 					config->SPLICED_OUT_FILE_NAME.c_str());
 			exit(1);
 		}
 	} else {
-		SP_OUT_FP = stdout;
+		SP_OUT_FP = OUT_FP ;
 	}
 
 	return (0);
