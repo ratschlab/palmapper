24751d9878292262159d39cbcc72fd8fb03fcc53
xdominik <xdominik@e1793c9e-67f9-0310-80fc-b846ff1f7b36>
2010-06-09 15:39:08 +0000

    * let the QueryFile count the reads

--- src/palmapper/QueryFile.h
+++ src/palmapper/QueryFile.h
@@ -24,10 +24,15 @@ public:
 		return _maxReadLen;
 	}
 
+	int read_count() const {
+		return _readCount;
+	}
+
 	static int determine_read_length(std::string const &filename);
 private:
 	FILE *_file;
 	int _lineNr;
 	unsigned int _maxReadLen;
 	std::string _filename;
+	int _readCount;
 };
