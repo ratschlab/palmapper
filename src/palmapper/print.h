dea7868304f083662518fefe713b39f8494f91dc
xdominik <xdominik@e1793c9e-67f9-0310-80fc-b846ff1f7b36>
2010-09-15 12:49:56 +0000

    * print results to memory before queueing them in FileReporter to allow earlier delete of the allocated structure

--- src/palmapper/print.h
+++ src/palmapper/print.h
@@ -8,6 +8,6 @@ extern char get_compl_base(char c);
 extern void print_stats(QueryFile &queryFile);
 extern int print_perfect_hits(unsigned int num);
 extern int print_largest_hit();
-extern void print_leftovers(Read const &read, const char *tag, FILE *LEFTOVER_FP);
+extern void print_leftovers(Read const &read, const char *tag, std::ostream *LEFTOVER_FP);
 extern void print_alignment_matrix(Read const &read, int chrstart, int readstart, int length, int offset_front, int offset_end, Chromosome const &chr, char ori, int K);
 extern int compare_int(const void *a, const void *b) ;
