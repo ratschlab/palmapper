2955edb86ac826539b19d5def0be5c53dea99dc3
gjean <gjean@e1793c9e-67f9-0310-80fc-b846ff1f7b36>
2011-08-24 12:42:11 +0000

    Variant cache structure: - Move variant cache structure in VariantMap - Add insertion field for variant cache str (give variant position for an insertion starting at this point. Insertion is unique)
    Improve speed up with variants:
    - Directly build variant cache when creating super sequence
    - Get rid of super variants usage in DP part -> template for myfast_align
    - Get positions of variants used at the end of DP (except used insertions)
    - Reconstruction alignment on reference from reported variant used and variant cache
    - Report only variants used and don't count twice an unbalanced substitution (bug fixed)

--- src/palmapper/dyn_prog/qpalma_dp.h
+++ src/palmapper/dyn_prog/qpalma_dp.h
@@ -14,7 +14,7 @@
 
 bool fast_result_align(const std::vector<SeedElem*>& seed_matrix_left, const std::vector<SeedElem*>& seed_matrix_right, int z, int est_len, int dna_len, int* result_length_ptr, 
 			char* est, char* dna, double* prb, int* s_align, int* e_align, int* mparam, double* alignmentscores, int* max_score_positions, 
-					   penalty_struct* qparam, mode currentMode, double score_seed,std::vector<FoundVariant>& vfound);
+					   penalty_struct* qparam, mode currentMode, double score_seed,std::vector<int>& vfound);
 
 
 
@@ -87,7 +87,7 @@ class Alignment {
       char * name;
       INT use_svm;
 	  
-	  std::vector<FoundVariant> fvariants;
+	  std::vector<int> fvariants;
 	  
 
    public:
@@ -99,16 +99,17 @@ class Alignment {
       void cleanup() ;
 	  double init_seed_position (int hit_read, int hit_dna, int hit_len, int& seed_i, int &seed_j, char* read, int read_len, char* dna, int d_len, struct penalty_struct* qualityScores, double* matchmatrix,int mm_len,double* prb);
 
+	  template<bool use_variants>
       void myalign_fast(char strand, Chromosome const &chr, int nr_paths_p, char* dna, int dna_len_p, char* est, int est_len_p, double* prb, struct penalty_struct h, double* matchmatrix, int mm_len,
 			 double* donor, int d_len, double* acceptor, int a_len, struct penalty_struct* qualityScores, 
 			 bool remove_duplicate_scores, int hit_read, int hit_dna, double best_match, int max_number_introns, 
-						int max_gap, int max_mism, int max_edit_op, int min_match,bool remapping,std::vector<SuperVariant> &super_variant_list, bool variant_mapping, int no_gap_end,int min_exom_len,int min_intron_len);
+						int max_gap, int max_mism, int max_edit_op, int min_match,bool remapping, bool variant_mapping, int no_gap_end,int min_exom_len,int min_intron_len, const std::vector<variant_cache_t *> &variant_cache);
 
       void getDNAEST();
       void getAlignmentResults(int* s_align, int* e_align,
 			       int* mmatrix_p, double* alignscores, double* qScores);
       int getResultLength() { return result_len; }
-	  std::vector<FoundVariant> getVariants() { return fvariants; }
+	  std::vector<int> getVariants() { return fvariants; }
       void getAlignmentArrays(int* dna_align, int* est_align);
       double scoreUnsplicedAlignment(const char * align_seq, double * prb, int read_length, struct penalty_struct* qualityScores, double* matchmatrix, char strand) ;
 };
