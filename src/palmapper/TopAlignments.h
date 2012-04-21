66ff2e560de0acae6458af3a6958ac08c61ff72b
raetsch <raetsch@e1793c9e-67f9-0310-80fc-b846ff1f7b36>
2012-04-21 23:37:16 +0000

    minor changes

--- src/palmapper/TopAlignments.h
+++ src/palmapper/TopAlignments.h
@@ -101,7 +101,7 @@ public:
 	int print_top_alignment_records_shorebed(Read const &read, std::ostream *OUT_FP, std::ostream *SP_OUT_FP) ;
 	int print_alignment_shorebed(Read const &read, std::ostream *OUT_FP, alignment_t* align, unsigned int num)  ;
 	int print_top_alignment_records_sam(Read const &read, std::ostream *OUT_FP, std::ostream *SP_OUT_FP) ;
-	int print_top_alignment_variants(std::ostream *OUT_FP, std::vector<Variant> v, const char * chr);
+	int print_top_alignment_variants(int rank, int total, std::ostream *OUT_FP, std::vector<Variant> v, const char * chr);
 	
 	static void print_bam_header(Genome& genome, FILE *OUT_FP);
 	static FILE* open_bam_pipe(std::string & out_fname) ;
