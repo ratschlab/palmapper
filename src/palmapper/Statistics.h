80a3a5274e300135b6fd00df89cadfde0e82c76b
Gunnar Raetsch <raetsch@cbio.mskcc.org>
2012-09-11 10:13:03 +0200

    improved output of variant_timing

--- src/palmapper/Statistics.h
+++ src/palmapper/Statistics.h
@@ -50,14 +50,20 @@ public:
 						alignment_num_unspliced_best, alignment_num_unspliced_suboptimal, alignment_num_spliced_best, alignment_num_spliced_suboptimal, alignment_num_unmapped,
 						100.0*alignment_num_spliced_best/(alignment_num_spliced_best+alignment_num_unspliced_best), 100.0*alignment_num_unmapped/(alignment_num_spliced_best+alignment_num_unspliced_best+alignment_num_unmapped)) ;
 
+				float sum1=variant_lower_bound_time+ variant_identification_time+ variant_create_super_sequence_from_variants_time+variant_myalign_fast_time+ variant_reconstruct_reference_alignment_time ;
+				
 				fprintf(stdout, "variant_timing:  lower_bound=%2.1f id=%2.1f create_super_sequence=%2.1f myalign_fast=%2.1f reconstruct_reference_alignment=%2.1f", 
-						variant_lower_bound_time, variant_identification_time, variant_create_super_sequence_from_variants_time, 
-						variant_myalign_fast_time, variant_reconstruct_reference_alignment_time) ;
+						variant_lower_bound_time/sum1, variant_identification_time/sum1, variant_create_super_sequence_from_variants_time/sum1, 
+						variant_myalign_fast_time/sum1, variant_reconstruct_reference_alignment_time/sum1) ;
 
+				float sum2=variant_create_super_sequence_from_variants_init_time+variant_create_super_sequence_from_variants_insertion_init_time+
+						variant_create_super_sequence_from_variants_table_init_time+variant_create_super_sequence_from_variants_table_fill1_time+
+					variant_create_super_sequence_from_variants_table_fill2_time + 1 ;
+				
 				fprintf(stdout, "create_super_sequence_timing:  init=%2.1f insert_init=%2.1f table_init=%2.1f fill1=%2.1f fill2=%2.1f", 
-						variant_create_super_sequence_from_variants_init_time, variant_create_super_sequence_from_variants_insertion_init_time,
-						variant_create_super_sequence_from_variants_table_init_time, variant_create_super_sequence_from_variants_table_fill1_time,
-						variant_create_super_sequence_from_variants_table_fill2_time) ;
+						variant_create_super_sequence_from_variants_init_time/sum2, variant_create_super_sequence_from_variants_insertion_init_time/sum2,
+						variant_create_super_sequence_from_variants_table_init_time/sum2, variant_create_super_sequence_from_variants_table_fill1_time/sum2,
+						variant_create_super_sequence_from_variants_table_fill2_time/sum2) ;
 			}
 		}
 
